990cc522996a65e957af79ee6e22f8e9ad0f0db7
Gabriel C Silva <gabriel.camposdasilva@hotmail.com>
2023-05-23 09:04:23 -0300

    crud products

---
+++ Migrations/20230513140817_NovoDBA.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GRazor.Migrations
+{
+    /// <inheritdoc />
+    public partial class NovoDBA : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,17 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddDbContext<GRazorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("GRazorContext") ?? throw new InvalidOperationException("Connection string 'GRazorContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'GRazorContext' not found.")));
 
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddDbContext<GRazorContext>(options =>
-        options.UseSqlite(builder.Configuration.GetConnectionString("GRazorContext")));
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 }
 else
 {
     builder.Services.AddDbContext<GRazorContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("ProductionGRazorContext")));
+        options.UseSqlServer(builder.Configuration.GetConnectionString("GRazorContext")));
 }
 var app = builder.Build();
 
